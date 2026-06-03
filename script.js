86b0f84e1aa8b9e936ff65d392111c8b1ca77843
doubleyip <syip428@gmail.com>
2026-06-02 21:58:41 -0400

    add feature to incorporate draw 1 and draw 2 cards

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 function getSelectedTurn(){
     return document.querySelector('input[name="option"]:checked').value;
-};
+}
 
 function getExpectedCopies(){
     return document.querySelector('input[name="expectedCopies"]').value;
-};
+}
 
 function getTurnCount(){
     return document.querySelector('input[name="turnCount"]').value;
 }
-function calculateCardCount(turn, expectedCopies, turnCount){
+
+function getDrawOneCount(){
+    return document.querySelector('input[name="drawOneCount"]').value;
+}
+
+function getDrawTwoCount(){
+    return document.querySelector('input[name="drawTwoCount"]').value;
+}
+
+function calculateCardCount(turn, expectedCopies, turnCount, drawOneCount, drawTwoCount){
     const startingHand = 5;
     const addedCards = turn === 1 ? turnCount : turnCount -1;
     const adjustedHandSize = startingHand + addedCards;
-    const copiesToPlay = expectedCopies * 50 / adjustedHandSize;
+    const drawOneModifier = drawOneCount !== 0? drawOneCount * adjustedHandSize / 50: 0;
+    const drawTwoModifier = drawTwoCount !== 0? (2 * drawTwoCount * adjustedHandSize) / 50: 0;
+    const copiesToPlay = expectedCopies * 50 / (adjustedHandSize + drawOneModifier + drawTwoModifier);
 
-
-    console.log(copiesToPlay);
-    return copiesToPlay;
+    return copiesToPlay.toFixed(2);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -25,15 +34,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const turn = getSelectedTurn() === "first" ? 1 : 2;
         const expectedCopies = Number(getExpectedCopies());
         const turnCount = Number(getTurnCount());
+        const drawOneCount = Number(getDrawOneCount());
+        const drawTwoCount = Number(getDrawTwoCount());
 
         let copiesToPlay = document.getElementById("copiesToPlay")
 
-        console.log({ turn, expectedCopies, turnCount });
-        copiesToPlay.textContent = (expectedCopies && turnCount)? calculateCardCount(turn, expectedCopies, turnCount): 0;
+        copiesToPlay.textContent = (expectedCopies && turnCount)? calculateCardCount(turn, expectedCopies, turnCount, drawOneCount, drawTwoCount): 0;
     }
 
     document.getElementById('turnSelectionRadio').addEventListener('change', updateInputs);
     document.getElementById('expectedCopies').addEventListener('change', updateInputs);
     document.getElementById('turnCount').addEventListener('change', updateInputs);
+    document.getElementById('drawOneCount').addEventListener('change', updateInputs);
+    document.getElementById('drawTwoCount').addEventListener('change', updateInputs);
     updateInputs();
 });
